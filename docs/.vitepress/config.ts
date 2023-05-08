830d79ba9ae98b82349ae2787346e92f51a98f27
duoli <zhaody901@126.com>
2023-05-08 18:40:26 +0800

    add swoole

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,103 +1,127 @@
 import { defineConfig } from "vitepress";
 
 const createLink = (title: string, url: string) => {
-	return { text: title, link: url };
+    return { text: title, link: url };
 };
 
 const nav = [
-	{
-		text: "语言",
-		activeMatch: `^/(dart)|(python)|(javascript)/`,
-		items: [
-			{ text: "Dart", link: "/dart/" },
-			{ text: "Python", link: "/python/" },
-			{ text: "Javascript", link: "/javascript/" },
-		],
-	},
-	{ text: "Web开发", link: "/web/" },
-	{ text: "Vitepress", link: "/vitepress/markdown-examples" },
+    {
+        text: "语言",
+        activeMatch: `^/(dart)|(python)|(javascript)/`,
+        items: [
+            { text: "Dart", link: "/dart/" },
+            { text: "Python", link: "/python/" },
+            { text: "Javascript", link: "/javascript/" },
+        ],
+    },
+    {
+        text: "Php",
+        activeMatch: `^/php/(swoole|hyperf)/`,
+        items: [
+            { text: "Swoole", link: "/php/swoole/" },
+            { text: "Hyperf", link: "/php/hyperf/" },
+        ],
+    },
+    { text: "Web开发", link: "/web/" },
+    { text: "Vitepress", link: "/vitepress/markdown-examples" },
 ];
 
 const sidebar = {
-	"/vitepress": [
-		{
-			text: "Examples",
-			items: [
-				createLink("Markdown", "/vitepress/markdown-examples"),
-				createLink("Api", "/vitepress/api-examples"),
-			],
-		},
-	],
-	"/web": [
-		{ text: "Web开发", link: "/web/" },
-		{
-			text: "Http",
-			items: [createLink("FAQ", "/web/http/faq.md")],
-		},
-	],
-	"/javascript": [
-		{
-			text: "入门",
-			items: [createLink("说明", "/javascript/")],
-		},
-	],
+    "/vitepress": [
+        {
+            text: "Examples",
+            items: [
+                createLink("Markdown", "/vitepress/markdown-examples"),
+                createLink("Api", "/vitepress/api-examples"),
+            ],
+        },
+    ],
+    "/web": [
+        { text: "Web开发", link: "/web/" },
+        {
+            text: "Http",
+            items: [createLink("FAQ", "/web/http/faq.md")],
+        },
+    ],
+    "/javascript": [
+        {
+            text: "入门",
+            items: [createLink("说明", "/javascript/")],
+        },
+    ],
+    "/php": [
+        {
+            text: 'Hyperf',
+            items: [
+                { text: '入门', link: '/php/hyperf/' },
+                { text: '注解', link: '/php/hyperf/core-annotation' },
+                { text: 'Aop', link: '/php/hyperf/core-aop' },
+            ]
+        },
+        {
+            text: 'Swoole',
+            items: [
+                { text: '入门', link: '/php/swoole/' },
+            ]
+        }
+    ],
 
-	"/python": [
-		{
-			text: "介绍",
-			items: [createLink("说明", "/python/")],
-		},
-		{
-			text: "入门",
-			items: [
-				createLink("问答环节", "/python/1_intro/1-qa.md"),
-				createLink("如何运行", "/python/1_intro/2-run.md"),
-			],
-		},
-		{
-			text: "类型和运算",
-			items: [
-				createLink("介绍 python 对象类型", "/python/2_types/4-types.md"),
-				createLink("数值类型", "/python/2_types/5-number.md"),
-			],
-		},
-		{
-			text: "Awesome",
-			items: [createLink("Pydash 中文文档", "/python/a_vendor/pydash.md")],
-		},
-	],
-	"/dart": [
-		{
-			text: "介绍",
-			items: [
-				createLink("说明", "/dart/"),
-				createLink("入门", "/dart/1_intro/"),
-				createLink("介绍", "/dart/1_intro/intro"),
-				createLink("命令", "/dart/1_intro/dart-command"),
-				createLink("pubspec", "/dart/1_intro/pubspec"),
-				createLink("FAQ", "/dart/1_intro/faq"),
-			],
-		},
-		{
-			text: "测试",
-			items: [createLink("单元测试", "/dart/7_tests/")],
-		},
-	],
+    "/python": [
+        {
+            text: "介绍",
+            items: [createLink("说明", "/python/")],
+        },
+        {
+            text: "入门",
+            items: [
+                createLink("问答环节", "/python/1_intro/1-qa.md"),
+                createLink("如何运行", "/python/1_intro/2-run.md"),
+            ],
+        },
+        {
+            text: "类型和运算",
+            items: [
+                createLink("介绍 python 对象类型", "/python/2_types/4-types.md"),
+                createLink("数值类型", "/python/2_types/5-number.md"),
+            ],
+        },
+        {
+            text: "Awesome",
+            items: [createLink("Pydash 中文文档", "/python/a_vendor/pydash.md")],
+        },
+    ],
+    "/dart": [
+        {
+            text: "介绍",
+            items: [
+                createLink("说明", "/dart/"),
+                createLink("入门", "/dart/1_intro/"),
+                createLink("介绍", "/dart/1_intro/intro"),
+                createLink("命令", "/dart/1_intro/dart-command"),
+                createLink("pubspec", "/dart/1_intro/pubspec"),
+                createLink("FAQ", "/dart/1_intro/faq"),
+            ],
+        },
+        {
+            text: "测试",
+            items: [createLink("单元测试", "/dart/7_tests/")],
+        },
+    ],
 };
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-	title: "语言学习",
-	description: "语言语法学习站",
-	themeConfig: {
-		search: {
-			provider: "local",
-		},
-		// https://vitepress.dev/reference/default-theme-config
-		nav,
+    title: "语言学习",
+    description: "语言语法学习站",
+    themeConfig: {
+        search: {
+            provider: "local",
+        },
+        // https://vitepress.dev/reference/default-theme-config
+        nav,
 
-		sidebar,
+        sidebar,
 
-		socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
-	},
+        socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+    },
 });
---
+++ docs/php/swoole/index.md
@@ -0,0 +1,66 @@
+# 介绍
+
+## 安装
+
+安装默认版本
+
+```
+$ pecl install swoole
+```
+
+安装其他版本, 所有的版本清单可以在 [PECL :: Package :: swoole (php.net)](https://pecl.php.net/package/swoole) 查找到
+
+![](https://file.wulicode.com/doc/20230508/1683541593696.png)
+
+
+
+```
+$ pecl install swoole-4.8.13
+```
+
+安装完毕之后的提示
+
+```
+Installing '/usr/local/Cellar/php@7.4/7.4.33_1/include/php/ext/swoole/php_swoole.h'
+Installing '/usr/local/Cellar/php@7.4/7.4.33_1/include/php/ext/swoole/config.h'
+Installing '/usr/local/Cellar/php@7.4/7.4.33_1/pecl/20190902/swoole.so'
+install ok: channel://pecl.php.net/swoole-4.8.12
+Extension swoole enabled in php.ini
+```
+
+## 相关支持
+
+在安装的时候会询问是否安装相关支持
+
+```
+enable sockets supports? [no] : 
+enable openssl support? [no] : 
+enable http2 support? [no] : 
+enable mysqlnd support? [no] : 
+enable json support? [no] : 
+enable curl support? [no] : 
+enable cares support? [no] : 
+```
+
+## 报错信息
+
+### openssl/ssl.h 缺失
+
+在安装 swoole 的时候会遇到错误信息如下：
+
+```
+....
+In file included from /private/tmp/pear/temp/swoole/php_swoole.h:53:
+/private/tmp/pear/temp/swoole/include/swoole.h:620:10: fatal error: 'openssl/ssl.h' file not found
+#include <openssl/ssl.h>
+         ^~~~~~~~~~~~~~~
+1 error generated.
+make: *** [php_swoole_cxx.lo] Error 1
+ERROR: `make' failed
+```
+
+没有加 openssl 库的路径或者指定 openssl 库的路径不对，缺少头文件。没有 openssl 的话使用 brew 安装一个 openssl，在 pecl 安装的时候加上对应路径即可
+
+```
+enable openssl support? [no] : yes --with-openssl-dir=/usr/local/opt/openssl@1.1
+```
